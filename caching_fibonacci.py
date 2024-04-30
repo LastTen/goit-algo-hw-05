0b1756331fbb5a47f5f9cd9c938cd8654c64cca8
Roman Stas <romastas1996@gmail.com>
2024-04-30 22:44:55 +0300

    first and  sec

---
+++ caching_fibonacci.py
@@ -0,0 +1,34 @@
+def caching_fibonacci():
+    """
+    This function returns a memoized fibonacci function that caches the results of previous calls to improve performance.
+
+    Returns:
+        A function that takes an integer n and returns the nth Fibonacci number.
+    """
+    cache = {}
+
+    def fibonacci(n: int):
+        """
+        This function calculates the nth Fibonacci number using dynamic programming.
+
+        Args:
+            n (int): The index of the Fibonacci number to calculate.
+
+        Returns:
+            int: The nth Fibonacci number.
+        """
+        if n in cache:
+            return cache[n]
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+
+if __name__ == "__main__":
+    print(caching_fibonacci()(0))
